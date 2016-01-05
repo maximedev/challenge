9bbfc45378500276068fdd0829689f318427721d
maximedev <maxime.desmauts@gmail.com>
2016-01-05 20:48:55 +0100

    ajout d'un tweet

---
+++ src/app/model/tweet/tweet.js
@@ -0,0 +1,39 @@
+(function(){
+  'use strict';
+
+  angular
+    .module('formationAngularLyon')
+    .service('TweetService',TweetService);
+
+  /** @ngInject */
+  function TweetService(DS,config,$http) {
+     var tweet = [] ;
+     
+     tweet.resource =function(){ 
+      return DS.defineResource('tweet', {
+      name: 'Tweet',
+      idAttribute: 'id',
+      relations: {
+        hasOne: {
+          User: {
+            localField: 'creator',
+            localKey: 'creatorId'
+          }
+        }
+      }
+    })
+
+    } 
+
+   tweet.createTweet =  function(tweet){
+        return $http({
+          method: 'POST',
+          url: config.api.basePath + "/add/tweet",
+          data: tweet
+        });
+      }
+
+     return tweet ;
+
+  }
+})();
