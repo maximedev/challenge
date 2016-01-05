0ecbce73999195fe79a64c3157462baca268b820
judirouillard <judikael.rouillard@gmail.com>
2016-01-05 15:52:00 +0100

    modifi tweet

--- src/app/views/twitter/twitter.controller.js
+++ src/app/views/twitter/twitter.controller.js
@@ -3,27 +3,75 @@
 
   angular
     .module('formationAngularLyon')
-    .controller('TwitterController', TwitterController);
+    .controller('TweetController', TweetController);
 
 
       /** @ngInject */
- function TwitterController(Todo,user,$log) {
+ function TweetController(list,Tweet,user,$log) {
 	var vm = this;
 
-	vm.submitForm = function (todo){
+  vm.list= list;
+
+        vm.submitForm = function(tweet){
+          $log.debug(tweet);
+          tweet.createdAt = new Date().getTime();
+          var promise;
+            tweet.creatorId = user.me.id;
+            promise = Tweet.create(tweet);
+
+          return promise.then(function(data){
+            return $mdDialog.hide(data);
+          })
+        }
+  /*function createTwit(twitter) {
+    $log.debug(twitter);
+    twitter.createdAt = new Date().getTime();
+    twitter.creatorId = user.me.id;
+    list.push(twitter);
+    return Twitter.create(twitter)
+      .then(function(twitter){
+      vm.list.push(twitter);
+      toastr.info('created Twitter !!!!')
+    });
+  }*/
+
+
+
+	/*function createTwit (todo){
 		$log.debug(todo);
-		todo.createdAt = new Date().getTime();
+    twitter.createdAt = new Date().getTime();
 		var promise;
 	if(todo.id){
 		promise= Todo.update(todo.id,todo);
 	}else{
-		todo.creatorId = user.me.id;
+    twitter.creatorId = user.me.id;
 		promise = Todo.create(todo);
       }
       return promise.then(function(data){
       $log.debug(data);
       })
-    }
+   }*/
 
+  function createTweet (tweet){
+    $log.debug(tweet);
+    toastr.info('created !!!!');
+
+    tweet.createdAt = new Date().getTime();
+    var promise;
+    tweet.creatorId = user.me.id;
+    promise = Tweet.create(tweet);
+    return promise.then(function(data){
+      $log.debug(data);
+    })
+  }
+
+  function ajoutTweet(tweet){
+    $log.debug(tweet);
+    vm.createTweet(tweet)
+      .then(function(createdTweet){
+      vm.list.push(createdTweet);
+      toastr.info('created !!!!')
+    })
+  }
   }
 })();
