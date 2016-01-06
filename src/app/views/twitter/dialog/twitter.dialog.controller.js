8cbdbaef99aa2aadce47518cf33523d3bed2c2c6
maximedev <maxime.desmauts@gmail.com>
2016-01-06 20:27:50 +0100

    Correction regression modification de l'affichage des tweets

--- src/app/views/twitter/dialog/twitter.dialog.controller.js
+++ src/app/views/twitter/dialog/twitter.dialog.controller.js
@@ -22,29 +22,17 @@
     vm.submitForm = submitForm;
     vm.close = close;
 
-    init();
-
-    function submitForm(tweet){
-      $log.debug("test submit form");
-      tweet.createdAt = new Date().getTime();
-      var promise;
-      tweet.creatorId = user.me.id;
-      promise = Tweet.create(tweet);
-      return promise.then(function(data){
-        return $mdDialog.hide(data);
-      })
+    function submitForm(tweetToPost){
+      $log.debug(tweetToPost);
+      tweetToPost.createdAt = new Date().getTime();
+      tweetToPost.creatorId = user.me.id;
+      return $mdDialog.hide(tweetToPost);
     }
 
     function close(){
       $mdDialog.cancel();
     }
 
-    function init(){
-
-      if(options.tweet){
-        vm.tweet = angular.copy(options.tweet);
-      }
-    }
   }
 
 })();
